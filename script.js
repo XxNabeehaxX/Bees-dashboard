3c7ad87caf13e3825cf3c7a88ad62f0d0d5b24b8
XxNabeehaxX <Nabeeha07@icloud.com>
2026-04-04 22:26:02 +0100

    Update script.js
    added hearts that fade away every time you pet him

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const benjy = document.getElementById("pet");
 
 function boop() {
+  const heart = document.createElement('div');
+  heart.textContent = '❤️';
+  heart.classList.add("heart"); 
+  heart.style.top= (80 + Math.random() * 80) + "px";
+  heart.style.left = (80 + Math.random() * 80) + "px";
+  heart.addEventListener('animationend', function() {
+  heart.remove();
+  });
+  document.body.appendChild(heart);
   console.log("Benjy was clicked!");
   if (benjy.src.includes("Benjy-no-boop.png")) {
   benjy.src = "Benjy-boop.png"
